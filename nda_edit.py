4dcfe0c33f8bda6f78633b1d79ccdabda58e5b6a
Ayo <t70256242@gmail.com>
2025-05-10 15:34:56 +0100

    friday Commit

--- nda_edit.py
+++ nda_edit.py
@@ -13,3 +13,12 @@ def nda_edit(input_path, output_path, context):
     print(f"{output_path} has been created!")
 
 
+
+# replacements_docx = {
+#                 "date": "22-3-1290",
+#                 "client_name": "       Diddy",
+#                 "client_company_name": "DAVE",
+#                 "client_company_address": "Ajalejo Street"
+#             }
+# nda_edit("penny_pig_2.docx", "wowo_8.docx", replacements_docx)
+
